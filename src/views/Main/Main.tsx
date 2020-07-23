753c1e19b5c7a9df78e61265d9bacb34a829cfea
Laura <laurabeatriserafim@gmail.com>
2020-07-22 22:33:27 -0300

    refactor: remove unnecessary content from the main page

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { KeyboardAvoidingView, Platform } from "react-native";
+import { SearchBar, WhiteSpace } from "@ant-design/react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 import { usePopup } from "../../contexts/popup/PopupContext";
-import {
-  Container,
-  Content,
-  BackgroundImage,
-  styles,
-} from "./styles";
 import { Title } from "../../styles/components/Typography";
 import Button from "../../components/Button/Button";
-import AddUserPopup from "../../components/AddUserPopup/AddUserPopup";
+import AddUserPopup from "../../components/AddUserPopup";
+import Bills from "../../components/Bills";
+import { Container, Content } from "./styles";
 
 const Main: React.FC = () => {
   const [openPopup] = usePopup();
@@ -23,29 +19,25 @@ const Main: React.FC = () => {
   };
 
   return (
-    <BackgroundImage
-      style={styles.backgroundImage}
-      // eslint-disable-next-line global-require
-      source={require("../../assets/background.png")}
-    >
-      <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        behavior={Platform.OS === "ios" ? "padding" : undefined}
-        enabled
-      >
-        <Container>
-          <Content>
-            <Title fontSize={32}>
-              Bill Splitting
-            </Title>
-          </Content>
 
-          <Button onPress={handlePopup}>
-            <Icon name="plus" size={24} />
-          </Button>
-        </Container>
-      </KeyboardAvoidingView>
-    </BackgroundImage>
+    <Container>
+      <Content>
+        <Title fontSize={32}>
+          Bill Splitting
+        </Title>
+
+        <WhiteSpace size="xl" />
+        <SearchBar
+          cancelText="Clear"
+          placeholder="Search for a user bill"
+        />
+        <Bills />
+      </Content>
+
+      <Button onPress={handlePopup}>
+        <Icon name="plus" size={24} />
+      </Button>
+    </Container>
   );
 };
 
