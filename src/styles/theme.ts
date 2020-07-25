6529dce1a4f1a07424b0906c484b918faff5e3ab
Laura <laurabeatriserafim@gmail.com>
2020-07-25 09:54:41 -0300

    chore: add new colors and font family to theme constant

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,12 +7,15 @@ const theme = {
     blue: "#1F83F2",
     white: "#FFF",
     grayLight: "#E6E9ED",
+    green: "#E7F6E0",
+    greenSecondary: "#96CA7F",
   },
   fonts: {
     families: {
       medium: "Roboto-Medium",
       regular: "Roboto-Regular",
       bold: "Roboto-Bold",
+      light: "Roboto-Light",
     },
     sizes: {
       base: 24,
